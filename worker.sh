f0a8c39a634991d30109a9c4ef22e0fd7313ee3d
CodeFreaker <1788436+codefreaker@users.noreply.github.com>
2020-05-03 14:44:00 +0530

    Create worker.sh

---
+++ worker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Installing keys and repositories for Docket and K8s"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+############################################
+echo "Install Docker and K8s"
+sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
+##################################################
+echo "setting up permissions"
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
